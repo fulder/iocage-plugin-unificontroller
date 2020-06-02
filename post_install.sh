f33ddb3eae89a5f1732e42fca9dd25943ae4209e
fulder <misad90@gmail.com>
2020-06-02 21:05:40 +0200

    Move back fetching poller to post_install

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,6 @@
 sysrc -f /etc/rc.conf unifi_enable="YES"
 service unifi start 2>/dev/null
 
-
-
+echo "Download and installing unifi-poller"
+fetch https://github.com/unifi-poller/unifi-poller/releases/download/v2.0.0/unifi-poller-2.0.0_760.amd64.txz
+pkg install -y unifi-poller-2.0.0_760.amd64.txz
