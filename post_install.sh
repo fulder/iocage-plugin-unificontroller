2de7aee06ce16d493b3e6c0bc0209d951f938f4b
fulder <misad90@gmail.com>
2020-06-02 21:05:57 +0200

    Remove downloaded poller package after installation

--- post_install.sh
+++ post_install.sh
@@ -6,3 +6,6 @@ service unifi start 2>/dev/null
 echo "Download and installing unifi-poller"
 fetch https://github.com/unifi-poller/unifi-poller/releases/download/v2.0.0/unifi-poller-2.0.0_760.amd64.txz
 pkg install -y unifi-poller-2.0.0_760.amd64.txz
+
+# Cleanup
+rm unifi-poller-2.0.0_760.amd64.txz
